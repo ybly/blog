84894bf42156fb6154e2cb8f9c78f6dd3cf8ee52
Bo <ybly.1993@gmail.com>
2024-05-13 19:01:38 +0100

    add layout to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Layout from '../components/Layout';
 
 export default function NotFound() {
 	return (
-		<div>
-			<h1>404</h1>
-			<p>Sorry, this page does not exist!</p>
-		</div>
+		<Layout>
+			<div>
+				<h1>404</h1>
+				<p>Sorry, this page does not exist!</p>
+			</div>
+		</Layout>
 	);
 }
