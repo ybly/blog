8ededc2a037dc62d83bef3195fc85e16035dba17
Bo <ybly.1993@gmail.com>
2024-05-13 21:21:26 +0100

    Add CSS Modules for specific page styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,22 @@
 import * as React from 'react';
 import Layout from '../components/Layout';
+import { Link } from 'gatsby';
+
+import * as styles from '../styles/home.module.css';
 
 export default function Home() {
 	return (
 		<Layout>
-			<section>
-				<h1>Home</h1>
-				<p>Hey! I'm Bo a Web Developer based in the West Midlands, UK.</p>
+			<section className={styles.header}>
+				<div>
+					<h2>Home</h2>
+					<h3>Web Developer</h3>
+					<p>Hey! I'm Bo a Web Developer based in the West Midlands, UK.</p>
+
+					<Link to="/projects" className={styles.btn}>
+						My Projects
+					</Link>
+				</div>
 			</section>
 		</Layout>
 	);
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Layout from '../../components/Layout';
 
+import * as styles from '../../styles/project.module.css';
+
 export default function Projects() {
 	return (
 		<Layout>
-			<section>
-				<h1>Projects</h1>
-				<p>My personal projects.</p>
+			<section className={styles.portfolio}>
+				<h2>Projects</h2>
+				<h3>My personal projects.</h3>
 			</section>
 		</Layout>
 	);
