718eecfe72e3f74a67c8e8735923e18082f2e2e9
Bo <ybly.1993@gmail.com>
2024-05-13 18:26:09 +0100

    Add Routes
    add about me and projects pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
-import * as React from "react"
+import * as React from 'react';
 
 export default function Home() {
-  return <div>Hello world!</div>
+	return (
+		<section>
+			<h1>HOME</h1>
+			<p>Hey! I'm Bo a Web Developer based in the West Midlands, UK.</p>
+		</section>
+	);
 }
