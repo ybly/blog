f75aacf37a2ba75c2cf059224e641b2468671c7b
Bo <ybly.1993@gmail.com>
2024-05-14 12:02:30 +0100

    update 'gatsby-transformer-remark' plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,17 @@ module.exports = {
 	plugins: [
 		{
 			resolve: `gatsby-transformer-remark`,
-			options: {},
+			options: {
+				// Footnotes mode (default: true)
+				footnotes: true,
+				// GitHub Flavored Markdown mode (default: true)
+				gfm: true,
+				// Add your gatsby-remark-* plugins here
+				plugins: [],
+				// Enable JS for https://github.com/jonschlinkert/gray-matter#optionsengines (default: false)
+				// It's not advised to set this to "true" and this option will likely be removed in the future
+				jsFrontmatterEngine: false,
+			},
 		},
 		{
 			resolve: `gatsby-source-filesystem`,
