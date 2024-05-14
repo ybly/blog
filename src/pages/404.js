29b31cf99996f00783e7d0a049b7b0281a048335
Bo <ybly.1993@gmail.com>
2024-05-14 10:23:53 +0100

    update 404 style

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,7 +5,11 @@ export default function NotFound() {
 	return (
 		<Layout>
 			<div>
-				<img src="404-error-bro-yellow.svg" alt="404 Error" />
+				<img
+					src="404-error-bro-yellow.svg"
+					alt="404 Error"
+					style={{ maxWidth: '100%' }}
+				/>
 			</div>
 		</Layout>
 	);
