14260c6a285744ea26b2c7287397810ef3d920a2
Bo <ybly.1993@gmail.com>
2024-05-16 17:42:55 +0100

    Add footer
    - Add footer disclaimers for icons and illustrations
    
    - Add social media links to footer

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -14,7 +14,57 @@ export default function Layout({ children }) {
 				<div>{children}</div>
 			</div>
 
-			<footer />
+			<footer>
+				<div>
+					<a
+						href="https://www.linkedin.com/in/yinboly"
+						target="_blank"
+						rel="noreferrer noopener"
+					>
+						<img
+							className="social-icon"
+							src="/icons8-linkedin.svg"
+							alt="icon-linkedin"
+						/>
+					</a>
+					<a
+						href="https://github.com/ybly"
+						target="_blank"
+						rel="noreferrer noopener"
+					>
+						<img
+							className="social-icon"
+							src="/icons8-github.svg"
+							alt="icon-github"
+						/>
+					</a>
+				</div>
+				<div>
+					<div className={style.disclaimer}>
+						<p>© 2024 Yin Bo Ly</p>
+						<p>
+							Illustrations by{' '}
+							<a
+								href="https://storyset.com/people"
+								target="_blank"
+								rel="noreferrer noopener"
+							>
+								Storyset
+							</a>
+						</p>
+						<p>
+							Icons by{' '}
+							<a
+								href="https://icons8.com"
+								target="_blank"
+								rel="noreferrer noopener"
+							>
+								Icons8
+							</a>
+						</p>
+					</div>
+				</div>
+			</footer>
 		</div>
 	);
 }
