03235f11ef4c1151ae36843579f4ecdbe84b9a4b
Bo <ybly.1993@gmail.com>
2024-05-16 13:52:28 +0100

    Add Button component

---
+++ src/components/Button/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+import * as style from './button.module.css';
+
+export function Button({ children, onClick = null }) {
+	return (
+		<button type="button" className={style.button} onClick={onClick}>
+			{children}
+		</button>
+	);
+}
+
+export function GatsbyLinkButton({ children, to }) {
+	return (
+		<Link to={to} className={style.button}>
+			{children}
+		</Link>
+	);
+}
